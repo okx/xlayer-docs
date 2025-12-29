1563c73f173ad9e453a915a0c3ff2c4ca44d8107
kai.zhang <kai.zhang@okg.com>
2025-12-29 17:59:48 +0800

    add icon

--- xlayer-docs.js
+++ xlayer-docs.js
@@ -141,6 +141,7 @@ module.exports = [
         },
         {
           title: 'Network Status',
+          icon: 'okx-defi-okbc-resources',
           externalLink: 'https://status.xlayer.tech',
           children: [],
         },
